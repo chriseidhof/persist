f24a1daa1c7b563c42d0c548b01f893b834ff5fe
Chris Eidhof <ce@tupil.com>
2010-08-26 10:01:23 +0200

    Got INSERT for sqlite working.

--- src/Data/Persist/Backend/Interface.hs
+++ src/Data/Persist/Backend/Interface.hs
@@ -1,16 +1,77 @@
-module Data.Persist.Backend.Interface where
+{-# LANGUAGE FlexibleContexts, TypeOperators, FlexibleInstances #-}
+module Data.Persist.Backend.Interface 
+  ( Ref (..)
+  , DBValue (..)
+  , create
+  , Persistent (..)
+  ) where
 
+import Control.Applicative
 import Generics.Regular
 import Data.Persist.AST
 
+--todo: also index ref by persistent class
 data Ref a = Ref Int
 
-class Persistent p where
-  create          :: (Regular record) 
-                  => record 
-                  -> p (Ref record)
-  addRelation :: (Regular from, Regular to) 
-              => Ref from 
-              -> Ref to
-              -> Relationship
-              -> p ()
+-- | A database value is always one of these types.
+data DBValue = DBString  String
+             | DBInt     Int
+             | DBBool    Bool
+             deriving (Show, Read)
+
+
+create :: (Regular a, DatabaseRepr (PF a), Persistent p) => a -> p (Ref a)
+create x = fmap Ref $ createImpl (tableName genX) (toDatabaseValue genX)
+ where genX = from x
+
+
+class Functor p => Persistent p where
+  createImpl       :: String              -- | The tableName
+                   -> [(String,DBValue)]  -- | Keys and values
+                   -> p Int               -- | The resulting id
+  addRelationImpl :: Int
+                  -> Int
+                  -> Relationship
+                  -> p ()
+
+
+
+-- Conversion code
+
+class DatabaseValue f => DatabaseRepr f where
+  tableName              :: f a -> String
+
+class DatabaseField f where
+  toField   :: f a -> DBValue
+  fromField :: DBValue -> Maybe (f a)
+
+class DatabaseValue f where
+  toDatabaseValue   :: f a -> [(String, DBValue)]
+  fromDatabaseValue :: [DBValue] -> Maybe (f a)
+
+instance (DatabaseValue f) => DatabaseValue (C c f) where
+  toDatabaseValue (C x) = toDatabaseValue x
+  fromDatabaseValue x   = C <$> fromDatabaseValue x
+
+instance (DatabaseValue f, DatabaseValue g) => DatabaseValue (f :*: g) where
+  toDatabaseValue (x :*: y) = toDatabaseValue x ++ toDatabaseValue y
+  fromDatabaseValue (x:xs)  = (:*:) <$> fromDatabaseValue [x] <*> fromDatabaseValue xs
+  fromDatabaseValue _       = Nothing
+
+instance (Selector s, DatabaseField f) => DatabaseValue (S s f) where
+  toDatabaseValue s@(S x) = [(selName s, toField x)]
+  fromDatabaseValue [x]   = S <$> fromField x
+  fromDatabaseValue _     = Nothing
+
+instance DatabaseField (K String) where
+  toField (K x)          = DBString x
+  fromField (DBString x) = Just (K x)
+  fromField _            = Nothing
+
+instance DatabaseField (K [Int]) where
+  toField (K x) = DBString $ show x
+  fromField (DBString x) = Just (K $ read x)
+  fromField _            = Nothing
+
+instance (Constructor c, DatabaseValue f) => DatabaseRepr (C c f) where
+  tableName           x = conName x
---
+++ src/Data/Persist/Backend/SQLite.hs
@@ -0,0 +1,101 @@
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE TemplateHaskell, EmptyDataDecls, TypeFamilies #-}
+module Data.Persist.Backend.SQLite where
+
+import Generics.Regular
+import Generics.Regular.TH()
+import Database.HDBC (quickQuery', fromSql, toSql, SqlValue (..), commit, disconnect)
+import Database.HDBC.Sqlite3 (Connection, connectSqlite3)
+import Data.Persist.Backend.Interface
+import Control.Monad.State
+import Data.Char (toLower)
+import Data.List (intercalate)
+
+-- Examples
+data Response = Response {
+  name    :: String,
+  email   :: String,
+  date    :: String,
+  answers :: [Int]
+}
+
+example :: Response
+example = Response "chris" "test" "date" [1,2,3]
+
+$(deriveAll ''Response "PFResponse")
+type instance PF Response = PFResponse
+
+data SQLiteState = SQLiteState {
+  conn :: Connection
+}
+
+newtype SQLite a = SQLite {unSQLite :: StateT SQLiteState IO a}
+
+instance Functor SQLite where
+  fmap f (SQLite a) = SQLite $ fmap f a
+
+instance Monad SQLite where
+  return = SQLite . return
+  (SQLite l) >>= r = SQLite $ l >>= (fmap unSQLite r)
+
+instance MonadIO SQLite where
+  liftIO = SQLite . liftIO
+
+sqliteLift :: StateT SQLiteState IO a -> SQLite a
+sqliteLift = SQLite
+
+debug :: String -> SQLite ()
+debug = liftIO . putStrLn
+
+instance Persistent SQLite where
+  createImpl tableName record = do
+    c <- sqliteLift (gets conn)
+    let query    = insertSQL tableName record
+        bindVals = tableSqlValues record
+    debug query
+    liftIO $ quickQuery' c query bindVals
+
+    [[rowId]] <- liftIO $ quickQuery' c "SELECT last_insert_rowid()" []
+      
+    return (fromSql rowId) -- TODO!
+    
+  -- addRelation :: (Regular from, Regular to) 
+  --             => Ref from 
+  --             -> Ref to
+  --             -> Relationship
+  --             -> p ()
+  addRelationImpl = undefined
+
+runSQLite :: String -> SQLite a -> IO a
+runSQLite dbName operation = do
+  c <- connectSqlite3 dbName
+  (result,_) <- runStateT (unSQLite operation) (SQLiteState c)
+  commit c
+  disconnect c
+  return result
+
+insertSQL :: String -> [(String,DBValue)] -> String
+insertSQL nm keysAndValues = unwords
+ [ "INSERT INTO "
+ , nm
+ , parens (commaList keys)
+ , "VALUES"
+ , parens (commaList $ map (const "?") keys)
+ ]
+ where (keys,_) = unzip keysAndValues
+
+tableSqlValues :: [(String, DBValue)] -> [SqlValue]
+tableSqlValues = map (toSqlValue . snd)
+ where toSqlValue (DBString s) = toSql s
+       toSqlValue (DBInt    s) = toSql s
+       toSqlValue (DBBool   s) = toSql s
+
+-- Utilities
+parens :: String -> String
+parens x = "(" ++ x ++ ")"
+
+commaList :: [String] -> String
+commaList = intercalate ","
+
+lower :: String -> String
+lower = map toLower
